daaf46a4094d3d60b8b56e192dbf7ea8d89dc689
Victor <victormarquesrina@hotmail.com>
2024-01-21 00:53:34 +0000

    Time status added

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,11 @@ const formattedDate = dayjs().format("dddd, D MMMM YYYY");
 
 // Set the formatted date in the header
 $("#currentDay").text(formattedDate);
+
+
+//Time status
+const getTimeStatus = (hour) => {
+    const currentHour = dayjs().hour();
+    return hour < currentHour ? "past" : hour === currentHour ? "present" : "future";
+  };
+  
